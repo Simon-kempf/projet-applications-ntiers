4cccc705ecac133333f6f61a8d63fbfb1b6b443f
simon <simon.kempf@uha.fr>
2023-10-22 13:05:06 +0200

    Ajout Contracts et Name

---
+++ JeBalance.Domain/JeBalance.Domain/Contracts/Specification.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace JeBalance.Domain.Contracts;
+
+public abstract class Specification<T>
+{
+    public abstract Expression<Func<T , bool>> ToExpression();
+ 
+    public bool IsSatisfiedBy(T entity)
+    {
+        Func<T , bool> predicate = ToExpression().Compile();
+        return predicate(entity);
+    }
+}
