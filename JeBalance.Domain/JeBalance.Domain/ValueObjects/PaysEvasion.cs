a599c32d2f7e8f59a7da1c2c930767cd6f548993
Thursaz <103246828+Thursaz@users.noreply.github.com>
2023-10-25 03:01:20 +0200

    Merge branch 'Vincent' of https://github.com/Simon-kempf/projet-applications-ntiers into Vincent

---
+++ JeBalance.Domain/JeBalance.Domain/ValueObjects/PaysEvasion.cs
@@ -0,0 +1,42 @@
+﻿using JeBalance.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeBalance.Domain.ValueObjects
+{
+    public class PaysEvasion : SimpleValueObject<String>
+    {
+        private const int MIN_LENGTH = 2;
+        private const int MAX_LENGTH = 50;
+
+        public PaysEvasion(string value) : base(value)
+        {
+        }
+
+        public override string Validate(string value)
+        {
+            var trimmedValue = value.Trim();
+
+            if (string.IsNullOrEmpty(trimmedValue)) throw new ApplicationException("Le nom d'un pays ne peut pas être vide");
+
+            if (trimmedValue.Length < MIN_LENGTH) throw new ApplicationException($"Le nombre de caractères du nom d'un pays doit être supérieur à  {MIN_LENGTH}");
+
+            if (trimmedValue.Length > MAX_LENGTH) throw new ApplicationException($"Le nombre de caractères du nom d'un pays doit être inférieur à {MAX_LENGTH}");
+
+            return trimmedValue;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Value == obj?.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Value);
+        }
+    }
+}
