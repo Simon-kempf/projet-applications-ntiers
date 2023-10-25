545fcb3e3490906f5b8960c5957bf861ffccb98d
NicolasCoudrillier <59061866+NicolasCoudrillier@users.noreply.github.com>
2023-10-25 10:14:06 +0200

    Création de CodePostal, NomDeCommune,NomDeVoie et NumeroDeVoie

---
+++ JeBalance.Domain/JeBalance.Domain/ValueObjects/NumeroDeVoie.cs
@@ -0,0 +1,32 @@
+﻿using JeBalance.Domain.Contracts;
+
+namespace JeBalance.Domain.ValueObjects;
+public class NumeroDeVoie : SimpleValueObject<int>
+{
+    private const int MIN = 1;
+    private const int MAX = 999;
+
+    public NumeroDeVoie(int value) : base(value)
+    {
+    }
+
+    public override int Validate(int value)
+    {
+
+        if (value < MIN) throw new ApplicationException($"Le numéro de voie ne peux pas être inférieur à {MIN}");
+
+        if (value > MAX) throw new ApplicationException($"Le numéro de voie ne peux pas être supérieur à {MAX}");
+
+        return value;
+    }
+
+    public static bool operator <(NumeroDeVoie a, int b) => a.Value < b;
+    public static bool operator >(NumeroDeVoie a, int b) => a.Value > b;
+    public static bool operator <=(NumeroDeVoie a, int b) => a.Value <= b;
+    public static bool operator >=(NumeroDeVoie a, int b) => a.Value >= b;
+    public static bool operator ==(NumeroDeVoie a, int b) => a.Value == b;
+    public static bool operator !=(NumeroDeVoie a, int b) => a.Value != b;
+
+    public override bool Equals(object? obj) => Value.Equals(obj);
+    public override int GetHashCode() => Value.GetHashCode();
+}
