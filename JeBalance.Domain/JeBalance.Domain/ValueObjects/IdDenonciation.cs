3c0c18f135a609fd8947ff25920c01f376e93f58
simon <simon.kempf@uha.fr>
2023-10-26 13:45:37 +0200

    Ajout IdDenonciation

---
+++ JeBalance.Domain/JeBalance.Domain/ValueObjects/IdDenonciation.cs
@@ -0,0 +1,29 @@
+﻿using JeBalance.Domain.Contracts;
+
+namespace JeBalance.Domain.ValueObjects;
+public class IdDenonciation : SimpleValueObject<int>
+{
+    private const int MIN = 0;
+
+    public IdDenonciation(int value) : base(value)
+    {
+    }
+
+    public override int Validate(int value)
+    {
+
+        if (value < MIN) throw new ApplicationException($"L'ID ne peut pas être inférieur à {MIN}");
+
+        return value;
+    }
+
+    public static bool operator <(IdDenonciation a, int b) => a.Value < b;
+    public static bool operator >(IdDenonciation a, int b) => a.Value > b;
+    public static bool operator <=(IdDenonciation a, int b) => a.Value <= b;
+    public static bool operator >=(IdDenonciation a, int b) => a.Value >= b;
+    public static bool operator ==(IdDenonciation a, int b) => a.Value == b;
+    public static bool operator !=(IdDenonciation a, int b) => a.Value != b;
+
+    public override bool Equals(object? obj) => Value.Equals(obj);
+    public override int GetHashCode() => Value.GetHashCode();
+}
