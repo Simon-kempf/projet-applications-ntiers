337bfae368c9fc57b28203d1a338f617f8fd26f1
simon <simon.kempf@uha.fr>
2023-10-26 13:35:29 +0200

    ajout Reponse et Type et ajout dans Denonciation

---
+++ JeBalance.Domain/JeBalance.Domain/Model/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeBalance.Domain.Model
+{
+    public enum Type
+    {
+        REJET = 0,
+        CONFIRMATION = 1
+    }
+}
