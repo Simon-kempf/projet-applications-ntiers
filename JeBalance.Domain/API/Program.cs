5ce65b8933f5da001eb2c3467d3765a54b9ae8f1
simon <simon.kempf@uha.fr>
2024-01-13 17:44:47 +0100

    erreur

--- JeBalance.Domain/API/Program.cs
+++ JeBalance.Domain/API/Program.cs
@@ -4,6 +4,7 @@ using JeBalance.Domain.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<ICategorieRepository, CategorieRepository>();
 builder.Services.AddSingleton<IDenonciationRepository, DenonciationRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
