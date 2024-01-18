5547ceee6117fa5ee1487bc1682940151e0aa775
simon <simon.kempf@uha.fr>
2024-01-18 13:39:03 +0100

    ajout SQLS

---
+++ JeBalance.Domain/JeBalance.Infrastructure/SQLServer/Model/ReponseSQLS.cs
@@ -0,0 +1,22 @@
+﻿using JeBalance.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeBalance.Infrastructure.SQLServer.Model
+{
+    public class ReponseSQLS
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("horodatage")]
+        public DateTime horodatage { get; set; }
+        [Column("type")]
+        public Domain.Model.Type type { get; set; }
+        [Column("retribution")]
+        public int retribution { get; set; }
+    }
+}
