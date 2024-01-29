8f7085e94c44f65d1d37f946f10c540df9c8cd34
simon <simon.kempf@uha.fr>
2024-01-29 13:15:29 +0100

    ajout Blazor

---
+++ JeBalance.Domain/JeBalance.UI/Authentication/UserSession.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace JeBalance.UI.Authentication
+{
+    public class UserSession
+    {
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+        public ClaimsPrincipal ToClaimsPrincipal(string? authenticationType = "CustomAuth")
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, UserName),
+                new Claim(ClaimTypes.Role, Role)
+            }, authenticationType));
+        }
+    }
+}
